1b65870517b448a116f3f5789769990f2e8eaac9
windowPC <drivespublic@gmail.com>
2022-02-08 23:01:07 +0900

    220208 chap02

--- src/test/java/org/choidh/toby_project/AppTests.java
+++ src/test/java/org/choidh/toby_project/AppTests.java
@@ -2,47 +2,61 @@ package org.choidh.toby_project;
 
 import org.choidh.toby_project.domain.User;
 import org.choidh.toby_project.domain.UserDao;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.GenericXmlApplicationContext;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = "/application-context-test.xml")
+//@DirtiesContext
 class AppTests {
     // AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-    // GenericXmlApplicationContext context = new GenericXmlApplicationContext("classpath:application-context.xml");
-    // ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application-context.xml", "ExamplePath.class);
-    ApplicationContext context = new GenericXmlApplicationContext("classpath:application-context.xml");
+    // GenericXmlApplicationContext context = new GenericXmlApplicationContext("classpath:application-context-test.xml");
+    // ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application-context-test.xml", "ExamplePath.class);
+    @Autowired private UserDao userDao;
+    private static User user_1;
+    private static User user_2;
+    private static User user_3;
+
+    @BeforeAll
+    public static void beforeAll() {
+        user_1 = new User("springex1", "CHOI", "1234");
+        user_2 = new User("springex2", "DONGHYUK", "4321");
+        user_3 = new User("springex3", "CHOIDONGHYUK", "9876");
+    }
 
     @BeforeEach
-    public void setup() throws Exception{
-        UserDao userDao = context.getBean("userDao", UserDao.class);
+    public void beforeEach() throws Exception {
+        // 아래처럼 쓰면 테스트 코드 속도가 엄청빨라짐
+        // DataSource dataSource = new SingleConnectionDataSource("jdbc:h2:~/toby", "sa", "", true);
+        // this.userDao.setDataSource(dataSource);
         userDao.deleteAll();
     }
 
     @Test
     @DisplayName("getCount() 검증")
-    void validGetCount() throws Exception{
-        UserDao userDao = context.getBean("userDao", UserDao.class);
+    void validGetCount() throws Exception {
         int result = userDao.getCount();
         assertEquals(result, 0);
 
-        User user_1 = new User("1", "choi", "test1");
         userDao.add(user_1);
         result = userDao.getCount();
         assertEquals(result, 1);
 
-        User user_2 = new User("2", "dong", "test2");
         userDao.add(user_2);
         result = userDao.getCount();
         assertEquals(result, 2);
 
-        User user_3 = new User("3", "hyuk", "test3");
         userDao.add(user_3);
         result = userDao.getCount();
         assertEquals(result, 3);
@@ -51,19 +65,15 @@ class AppTests {
     @Test
     @DisplayName("더하고 가져오고")
     void addAndGet() throws SQLException {
-        User user_1 = new User("springex1", "CHOI", "1234");
-        User user_2 = new User("springex2", "DONGHYUK", "4321");
+        assertEquals(userDao.getCount(), 0);
 
-        UserDao userDAO = context.getBean("userDao", UserDao.class);
-        assertEquals(userDAO.getCount(), 0);
+        userDao.add(user_1);
+        userDao.add(user_2);
 
-        userDAO.add(user_1);
-        userDAO.add(user_2);
-
-        int after = userDAO.getCount();
+        int after = userDao.getCount();
         assertEquals(after, 2);
 
-        User newUser = userDAO.get("springex1");
+        User newUser = userDao.get("springex1");
 
         assertEquals(newUser.getName(), user_1.getName());
         assertNotEquals(newUser.getName(), user_2.getName());
@@ -75,23 +85,19 @@ class AppTests {
     @Test
     @DisplayName("더하고 가져오고 ( 에러 발생 )")
     void addAndGet_Exception() throws SQLException {
-        User user_1 = new User("springex1", "CHOI", "1234");
-        User user_2 = new User("springex2", "DONGHYUK", "4321");
+        assertEquals(userDao.getCount(), 0);
 
-        UserDao userDAO = context.getBean("userDao", UserDao.class);
-        assertEquals(userDAO.getCount(), 0);
+        userDao.add(user_1);
+        userDao.add(user_2);
 
-        userDAO.add(user_1);
-        userDAO.add(user_2);
-
-        int after = userDAO.getCount();
+        int after = userDao.getCount();
         assertEquals(after, 2);
 
         assertThrows(EmptyResultDataAccessException.class, () -> {
-            userDAO.get("unknown_id");
+            userDao.get("unknown_id");
         });
 
-        User newUser = userDAO.get("springex1");
+        User newUser = userDao.get("springex1");
 
         assertEquals(newUser.getName(), user_1.getName());
         assertNotEquals(newUser.getName(), user_2.getName());
---
+++ src/test/java/org/choidh/toby_project/AppTests_3.java
@@ -0,0 +1,35 @@
+package org.choidh.toby_project;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = "/application-context-test.xml")
+class AppTests_3 {
+
+    @Autowired
+    ApplicationContext context;
+
+    @Test
+    @DisplayName("Context 값 확인_1")
+    public void test1() {
+        System.out.println(context);
+    }
+
+    @Test
+    @DisplayName("Context 값 확인_2")
+    public void test2() {
+        System.out.println(context);
+    }
+
+    @Test
+    @DisplayName("Context 값 확인_3")
+    public void test3() {
+        System.out.println(context);
+    }
+}
