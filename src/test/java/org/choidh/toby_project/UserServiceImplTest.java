7bbc5aeba73dac32c21b5544741f7f6131ace847
windowPC <drivespublic@gmail.com>
2022-03-17 10:20:21 +0900

    cm

--- src/test/java/org/choidh/toby_project/UserServiceImplTest.java
+++ src/test/java/org/choidh/toby_project/UserServiceImplTest.java
@@ -14,7 +14,6 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.dao.TransientDataAccessResourceException;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
-import org.springframework.stereotype.Service;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.Transactional;
@@ -351,8 +350,7 @@ public class UserServiceImplTest extends TestConfig{
 
     }
 
-    @Service("txAnnotation")
-    class TxAnnotationUser extends UserServiceImpl {
+    static class TxAnnotationUser extends UserServiceImpl {
 
         @Override
         @Transactional(readOnly = true)
